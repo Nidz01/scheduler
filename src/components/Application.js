f1ee06436cd83ccfcef9e891ef74552f243d39b3
Nida.Q <nida@emamilid@gmail.com>
2020-08-05 01:19:01 -0400

    Rendered a Schedule of Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,66 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,	    
+    time: "12pm"	    
+  },
+  {
+    id: 2,	
+    time: "1pm",	
+    interview: {	
+      student: "Lydia Miller-Jones",	
+      interviewer: {	
+        id: 1,	
+        name: "Sylvia Palmer",	
+        avatar: "https://i.imgur.com/LpaY82x.png"	
+      }	
+    }	
+  },	
+  {	
+    id: 3,	
+    time: "2pm",	
+    interview: {	
+      student: "Noah Novick",	
+      interviewer: {	
+        id: 2,	
+        name: "Tori Malcolm",	
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"	
+      }	
+    }	
+  },	
+  {	
+    id: 4,	
+    time: "3pm",	
+    interview: {	
+      student: "David He",	
+      interviewer: {	
+        id: 3,	
+        name: "Mildred Nazir",	
+        avatar: "https://i.imgur.com/T2WwVfS.png"	
+      }	
+    }	
+  },	
+  {	
+    id: 5,	
+    time: "4pm",	
+    interview: {	
+      student: "Lydia Miller-Jones",	
+      interviewer: {	
+        id: 4,	
+        name: "Cohana Roy",	
+        avatar: "https://i.imgur.com/FK8V841.jpg"	
+      }	
+    }	
+  },	
+  {	
+    id: "last",	
+    time: "5pm"	
+  }	
+];
 
 
 const days = [
@@ -22,7 +82,12 @@ const days = [
   },
 ];
 
-
+const appointmentList = appointments.map(appointment => (
+  <Appointment key={appointment.id} {...appointment}>	
+    <h4 className="interviewers__header text--light">interviewer</h4>	
+    <ul className="interviewers__list"></ul>	
+  </Appointment>	
+))
 
 export default function Application(props) {
 
@@ -48,7 +113,7 @@ export default function Application(props) {
         alt="Lighthouse Labs"/>
         </section>
         <section className="schedule">
-          {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+          {appointmentList}
         </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+
+
+
 import "./styles.scss";
 
-export default function Appointment () {
+export default function Appointment (props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}></Header>
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
   )
 }
