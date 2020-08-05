e9ce0fdbc7307b1a7e68f21dde67dc83783285c5
Nida.Q <nida@emamilid@gmail.com>
2020-08-05 00:36:29 -0400

    Added state to Appointment Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -16,28 +16,32 @@ export default function Form (props) {
     reset();
     return props.onCancel();
   };
-  
+
+  const save = () => {
+    return props.onSave(name, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => {event.preventDefault(); }} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name={name}
             type="text"
             placeholder="Enter Student Name"
             value={name}
-            onChange={e => {
-              setName(e.target.value);
+            onChange={event => {
+              setName(event.target.value);
             }}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.value} onChange={(event) =>setInterviewer(event.target.value)} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
