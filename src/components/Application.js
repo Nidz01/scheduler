aed90834eb38dfae5acf8ebd1aa72334f7b016d7
Nida.Q <nida@emamilid@gmail.com>
2020-08-05 22:53:17 -0400

    Fecthed appointment data through promise

--- src/components/Application.js
+++ src/components/Application.js
@@ -81,13 +81,16 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  useEffect (()=> {
-    axios.get(`/api/days`).then(response => {
-      setDays(response.data)
+  useEffect(() => {
+    Promise.all([
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`)
+    ]).then(all => {
+      console.log(all[0])
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
     })
-  }, [])
+  }, []);
 
   return (
     <main className="layout">
