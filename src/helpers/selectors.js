159d17890f111f19854c673345edb18b0f0f72de
Nida.Q <nida@emamilid@gmail.com>
2020-08-05 20:06:34 -0400

    Transformed data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+  const foundDay = state.days.find(stateDay => day === stateDay.name);
+  if (!foundDay) {
+    return [];
+  }
+  const foundAppointments = foundDay.appointments.map(appointment => {
+    return state.appointments[appointment];
+  });
+  return foundAppointments;
+}
