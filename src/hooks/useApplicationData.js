5ef977790a6ccda78bf98c00d53b9672c2669641
Nida.Q <nida@emamilid@gmail.com>
2020-08-13 13:15:03 -0400

    Refactored code and Added screenshots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -23,6 +23,35 @@ export function useApplicationData() {
 
   const setDay = day => setState({ ...state, day });
 
+  const spotsRemaing = (state,appointments) => {
+    let appointmentsArray = [];
+    //Creating array with one selected day Object
+    const filteredDays = state.days.filter(dayItem => dayItem.name === state.day);
+    //Creating array with the appointments objects for the selected day
+    filteredDays[0].appointments.forEach((element) => {
+        appointmentsArray.push(appointments[element]);
+    })
+    //Creating new array of the interview objects that have interview set to null
+    let remainingSpots = 0;
+    appointmentsArray.forEach((appointmentObj) => {
+        if(!appointmentObj.interview) {
+            remainingSpots++
+        }
+        return remainingSpots
+    })
+    //Creating newDays array that has days array with updated spots
+    let newDays = [];
+    state.days.forEach((dayObj) => {
+        if(dayObj.name === state.day){
+            dayObj.spots = remainingSpots
+            newDays.push(dayObj);
+        } else {
+            newDays.push(dayObj);
+        }
+    })
+    return newDays;
+  }
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -34,37 +63,31 @@ export function useApplicationData() {
       [id]: appointment
     };
 
-    return axios.put(`api/appointments/${id}`, appointment).then(res => {
+    return axios.put(`api/appointments/${id}`, appointment).then(() => {
       setState({
         ...state,
         appointments,
+        days: spotsRemaing(state, appointments)
       })
-      axios.get("/api/days")
-      .then((res)=>{
-        setState(prev => ({ ...prev, days: res.data}));
-      })
-      return res;
     })
   }
 
   function cancelInterview(id) {
     const appointment = {
-      ...state.appointments[id]
+      ...state.appointments[id],
+      interview: null
     }
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-    return axios.delete(`api/appointments/${id}`).then(res => {
+
+    return axios.delete(`api/appointments/${id}`).then(() => {
       setState({
         ...state,
         appointments,
-      })
-      axios.get("/api/days")
-      .then((res)=>{
-        setState(prev => ({ ...prev, days: res.data}));
-      })
-      return res;
+        days: spotsRemaing(state, appointments)
+      });
     })
   }
 
